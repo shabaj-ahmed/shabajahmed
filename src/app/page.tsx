1358fbc36e552053119b2e1605b9bf1ded319249
msahmed1 <shabaj_ahmed@outlook.com>
2025-04-17 19:21:46 +0100

    Updated home page content to provide a overview of skills, projects, and collaboration opportunities

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,44 @@
 export default function HomePage() {
   return (
     <div>
-      <h1 className="text-2xl font-bold">Welcome to My Site!</h1>
-      <p>This is the home page summarising my experience and hobbies.</p>
+      <h1 className="text-2xl font-bold"> 🌱 About Me</h1>
+      <p>
+      From as far back as I can remember, my path has been shaped by a deep-rooted curiosity and a relentless pursuit to solve problems. While my professional journey is varied, spanning automotive, startups, and academia, the common thread is my commitment to creating meaningful solutions that bring value to people. For me, it is about rolling up my sleeves, diving deep into a challenge and coming up with something impactful on the other end. The technology I use or the accolades I receive are a side effect and not the objective.
+      <br /><br />
+      Every challenge I tackle is an opportunity to learn and grow, adding new tools to my toolkit. Whether it's a new technology, a novel methodology, or an efficient process. When faced with a new problem, I carefully select the best tool to solve the problem. I'm not an "expert" in any domain, but I believe that's my strength. Being a 'jack of all trades' means I bring a unique, cross-disciplinary perspective to the table, allowing me to connect dots others might miss.
+      </p>
+
+      <h1 className="text-2xl font-bold mt-6"> 🛠 Skills and Expertise</h1>
+      <ul>
+        <li><strong>Cross-Disciplinary Knowledge</strong>: Mechanical engineering, electronics and software development</li>
+        <li><strong>Team Collaboration</strong>: Although I am self-motivated to pursue projects, the projects I enjoy the most involve collaborating with diverse teams.</li>
+        <li><strong>Adaptable Learner</strong>: Continuously exploring new technologies and approaches, applying them pragmatically to solve the appropriate problems</li>
+      </ul>
+
+      <h1 className="text-2xl font-bold mt-6"> ⚙️ Current Work and Skills</h1>
+      <ul>
+        <li><strong>Current PhD project</strong>: I am working on the final study to complete my PhD. This project involves deploying a robot in the wild for multiple weeks to investigate if it can help someone form a healthy habit.</li>
+      </ul>
+
+      <h1 className="text-2xl font-bold mt-6"> 💼 Professional Highlights</h1>
+      <ul>
+        <li><strong>PhD Research at Bristol University</strong>: Advancing the field of human-robot interaction.</li>
+        <li><strong>Co-founder at 3D Farms</strong>: Led the development of an automated aeroponics warehouse.</li>
+        <li><strong>ADAS System Architect at Jaguar Land Rover</strong>: Managed and coordinated autonomous driving signals between ECUs</li>
+      </ul>
+
+      <h1 className="text-2xl font-bold mt-6"> 🏆 Key Projects and Achievements</h1>
+      <ul>
+        <li><strong>BrisHacks Hackathon 2024 Winner</strong>: To tackle childhood obesity, we created a video game that requires players to engage in physical activities to control the character on screen.</li>
+        <li><strong>Robotic + Care Hackathon 2023 Winner</strong>: Developed a social care robot to aid in healthcare settings.</li>
+        <li><strong>Progressable</strong>: A cross-platform exercise journal application developed using Flutter. Download it at <a href="https://www.progressable.co.uk" target="_blank">www.progressable.co.uk</a>.</li>
+      </ul>
+
+      <h1 className="text-2xl font-bold mt-6"> 🎨 Hobbies and Interests</h1>
+      <p>Outside the PhD, I enjoy a balanced lifestyle with interests ranging from fitness and photography to DIY.</p>
+
+      <h1 className="text-2xl font-bold mt-6"> 🤝 Looking for Collaborations</h1>
+      <p>I am open to collaborations involving Human-Robot interaction or health technology, particularly projects aiming to enhance or augment human performance or improve health span. Please feel free to reach out if you want to talk through an idea with me or if you have a project that you want help with.</p>
     </div>
   );
 }
